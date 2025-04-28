82bd1f27778294f21259114f06980ce6b7597ca8
aais1 <aaisali228@gmail.com>
2025-04-28 22:28:40 +0500

    controllers && services && modules

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,35 +1,44 @@
 import { Controller, Delete, Get, Patch, Post, Param, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './interfaces';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
   // GET /users
   // GET /users/:id
   // POST /users
   // PATCH /users/:id
   // DELETE /users/:id
 
-  @Get()
-  findAll(): any[] {
-    return [];
+  @Get('/test')
+  test() {
+    return 'test';
+  }
+
+  @Get('')
+  findAll() {
+    return this.userService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string): any[] {
-    return [id];
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
   }
 
   @Post()
-  createOne(@Body() user: object) {
-    return user;
+  createOne(@Body() user: User) {
+    return this.userService.createOne(user);
   }
 
-  @Patch(':id') //will updating the user with id and will also include the body in req
-  updateOne(@Param('id') id: number, @Body() user: object): object {
-    return { id, ...user };
+  @Patch(':id') //will update the user with id and req will also include the body that will contain user
+  updateOne(@Param('id') id: string, @Body() user: User) {
+    return this.userService.updateOne(user);
   }
 
   @Delete(':id')
-  deleteOne(@Param('id') id: string): any[] {
-    return [id];
+  deleteOne(@Param('id') id: string) {
+    return this.userService.deleteOne(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './interfaces';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users: User[] = [{ id: '1', name: 'John Doe', email: 'john@gmail.com', role: 'INTERN' }];
+
+  findAll() {
+    return this.users;
+  }
+  findOne(id: string) {
+    return this.users.find((user) => user.id === id);
+  }
+  createOne(user: User) {
+    const newUser: User = {
+      id: (this.users.length + 1).toString(),
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+  updateOne(user: User) {
+    return { ...user };
+  }
+  deleteOne(id: string) {
+    return { id };
+  }
+}
